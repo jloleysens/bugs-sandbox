abf79d91f8e156fd740cad4a801b8d6d5b833c11
Jean-Louis Leysens <jloleysens@gmail.com>
2019-11-11 16:29:42 +0100

    EuiCodeEditor component error log

---
+++ eui-code-editor/app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '@elastic/eui/dist/eui_theme_light.css'
+import {EuiCodeEditor} from '@elastic/eui';
+
+function App() {
+  return (
+    <EuiCodeEditor />
+  );
+}
+
+export default App;
